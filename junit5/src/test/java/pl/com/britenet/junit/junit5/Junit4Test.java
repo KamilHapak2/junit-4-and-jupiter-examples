e1218e1bca00bd4a0abd418a1c803485545f5ea7
kamil.hapak <kamil.hapak@britenet.com.pl>
2018-03-03 16:52:11 +0100

    Dodaj JUnit-vintage do modułu z JUnit5

---
+++ junit5/src/test/java/pl/com/britenet/junit/junit5/Junit4Test.java
@@ -0,0 +1,17 @@
+package pl.com.britenet.junit.junit5;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Junit4Test {
+
+    @Before
+    public void init() {
+
+    }
+
+    @Test
+    public void name() throws Exception {
+
+    }
+}
