f542c3a455eb83fb42c389bd8507156e139f6107
kamil.hapak <kamil.hapak@britenet.com.pl>
2019-12-12 12:09:29 +0100

    Before and after test

---
+++ junit5/src/test/java/pl/com/britenet/junit/junit5/BeforeAndAfterTest.java
@@ -0,0 +1,43 @@
+package pl.com.britenet.junit.junit5;
+
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.*;
+
+@Log
+class BeforeAndAfterTest {
+
+  @BeforeAll
+  static void beforeAll() {
+    log.info("Before all");
+  }
+
+  @AfterAll
+  static void afterAll() {
+    log.info("After all");
+  }
+
+  @AfterEach
+  void afterEach() {
+    log.info("After each");
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    log.info("Before each");
+  }
+
+  @Test
+  void test1() {
+    log.info("test1 is being executed");
+  }
+
+  @Test
+  void test2() {
+    log.info("test2 is being executed");
+  }
+
+  @Test
+  void test3() {
+    log.info("test3 is being executed");
+  }
+}
